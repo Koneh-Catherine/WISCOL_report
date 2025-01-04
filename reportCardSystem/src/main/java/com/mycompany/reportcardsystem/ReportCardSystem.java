bea4961e43cb1f49892858db160fbd2f96b5fc2e
Asongna-frank <87443470+Asongna-frank@users.noreply.github.com>
2025-01-05 00:18:54 +0100

    initial commit

---
+++ reportCardSystem/src/main/java/com/mycompany/reportcardsystem/ReportCardSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.reportcardsystem;
+
+/**
+ *
+ * @author ASONGNA FRANK
+ */
+public class ReportCardSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
