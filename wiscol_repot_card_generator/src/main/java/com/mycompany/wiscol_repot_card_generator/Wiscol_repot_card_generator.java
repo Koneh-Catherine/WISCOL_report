9a2ee1141b09856d10b98ca9fc629be6ce74d500
Asongna-frank <asongnafrank@gmail.com>
2025-02-03 15:02:24 +0100

    Initial commit

---
+++ wiscol_repot_card_generator/src/main/java/com/mycompany/wiscol_repot_card_generator/Wiscol_repot_card_generator.java
@@ -0,0 +1,12 @@
+package com.mycompany.wiscol_repot_card_generator;
+
+/**
+ *
+ * @author ASONGNA FRANK
+ */
+public class Wiscol_repot_card_generator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
