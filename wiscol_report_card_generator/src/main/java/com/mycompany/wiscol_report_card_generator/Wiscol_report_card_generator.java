0d98389f3133b2dad3b67f0fa217cd2ffaa5c277
Asongna-frank <asongnafrank@gmail.com>
2025-02-18 15:04:58 +0100

    Added the dotenv-java and postgresqll JDBC dependencies

--- wiscol_report_card_generator/src/main/java/com/mycompany/wiscol_report_card_generator/Wiscol_report_card_generator.java
+++ wiscol_report_card_generator/src/main/java/com/mycompany/wiscol_report_card_generator/Wiscol_report_card_generator.java
@@ -1,10 +1,10 @@
-package com.mycompany.wiscol_repot_card_generator;
+package com.mycompany.wiscol_report_card_generator;
 
 /**
  *
  * @author ASONGNA FRANK
  */
-public class Wiscol_repot_card_generator {
+public class Wiscol_report_card_generator {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
